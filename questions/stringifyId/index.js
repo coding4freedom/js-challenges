fbbb0017be5b88ece5c35e3086b174ef37dd3b68
chris hawkins <chawkins2010@hotmail.com>
2024-11-15 12:56:03 -0600

    stringifyId notes and solution

--- questions/stringifyId/index.js
+++ questions/stringifyId/index.js
@@ -17,7 +17,44 @@
  */
 
 const stringifyId = (arr) => {
+    const idArray = arr.map(obj => obj.id);
+    const uniqueArray = [...new Set(idArray)]; // set removes dup values
+    
 
+    return uniqueArray.join(', ');
 };
 
 module.exports = stringifyId;
+
+/*
+ - convert the array of Object into an Array of IDs
+ - convert the array of IDs into an Array of unique IDs
+ - join the array to create a string of IDs separated by ','
+
+ example of the brute force way
+
+ const stringifyId = (arr) => {
+    const idArray = arr.map(obj => obj.id);
+    const uniqueArray = [];
+    let idString = '';
+
+    for(let id of idArray) {
+        if (!uniqueArray.includes(id)) {
+            uniqueArray.push(id);
+        }
+    }
+
+    for (let i = 0; i < uniqueArray.length; i++) {
+        // if we are not on the last element
+        if (i !== uniqueArray.length - 1) {
+            idString += `${uniqueArray[i]}, `
+        } else {
+            idString += uniqueArray[i];
+        }
+        // add the id and ','
+        // else
+        //   just add id
+    }
+    return idString;
+ };
+*/
